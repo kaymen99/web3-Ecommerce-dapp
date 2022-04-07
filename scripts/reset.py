ffae76cf3f630fe4ec5270f20356bdea1eab632a
Aymen <AymenMir1001@gmail.com>
2022-04-07 16:21:40 +0100

    Update reset.py

--- scripts/reset.py
+++ scripts/reset.py
@@ -5,8 +5,9 @@ import os, shutil
 """
 
 
-def reset():
+def reset_depoyments():
     folder = "./build/deployments"
+
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
         try:
@@ -18,5 +19,34 @@ def reset():
             print("Failed to delete %s. Reason: %s" % (file_path, e))
 
 
+def reset_compiled_contracts():
+    folder = "./build/contracts"
+
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+
+def reset_compiled_interfaces():
+    folder = "./build/interfaces"
+
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
+
+            
 def main():
-    reset()
+    reset_depoyments()
+    reset_compiled_contracts()
+    reset_compiled_interfaces()
